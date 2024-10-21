b0d2c6c5366e1e9c67786788a40afa27cbcb370d
KILLinefficiency <shreyas.sable2166@gmail.com>
2024-10-21 11:58:22 +0530

    added ui for the editor page

--- src/routes/editor/[id]/+page.svelte
+++ src/routes/editor/[id]/+page.svelte
@@ -1,11 +1,18 @@
 <script>
     import { onMount } from "svelte";
 
+    import CodeMirror from "svelte-codemirror-editor";
+    import { python } from "@codemirror/lang-python";
+    import { oneDark } from "@codemirror/theme-one-dark";
+
+    let code = "";
+
     import Appbar from "$lib/Appbar.svelte";
     const user = localStorage.getItem("codeCraftUser");
     export let data;
 
     let problem = [];
+    let testcases = [];
     onMount(() => {
         fetch(`/editor/${data.id}`, {
             method: "POST",
@@ -19,17 +26,76 @@
         .then(body => {
             console.log(body);
             problem = body;
+            testcases = body.testcases;
         });
     });
 </script>
 
 <style>
+    .main {
+        display: grid;
+        grid-template-columns: repeat(2, 50%);
+    }
     h1 {
+        padding: 40px;
         color: white;
         font-size: 30px;
+    }
+    h2 {
+        padding: 40px;
+        color: white;
+        font-size: 20px;
+    }
+    .buttons {
+        display: flex;
+        flex-direction: row;
+        gap: 20px;
+        justify-content: center;
+        align-items: center;
+        padding: 20px 0px;
+    }
+    button {
+        width: 95px;
+    }
+    .cases {
+        margin: 20px 0px;
+        display: flex;
+        flex-direction: row;
+        gap: 20px;
+    }
+    .each-case {
+        @apply variant-filled-primary;
     }
 </style>
 
 <Appbar {user} />
 
-<h1>{problem.title}</h1>
+<div class = "main">
+    <div>
+        <h1>{problem.title}</h1>
+        <h2>{problem.problem}</h2>
+    </div>
+
+    <div>
+        <CodeMirror
+            bind:value={code}
+            lang={ python() } 
+            theme={ oneDark }
+            styles={{
+                "&": {
+                    width: "100%",
+                    height: "60vh"
+                }
+            }}
+        />
+        <div class = "buttons">
+            <button class = "btn variant-filled-primary">Run</button>
+            <button class = "btn variant-filled-success">Submit</button>
+        </div>
+        <div class = "cases">
+            {#each testcases as eachCase, idx}
+                <div class = "each-case p-4">{idx + 1}. Pending</div>
+            {/each}
+        </div>
+    </div>
+</div>
