ec780cbf189553dc7ebca59c45a43274ba86e6e9
KILLinefficiency <shreyas.sable2166@gmail.com>
2024-10-24 23:47:06 +0530

    isolated docker part for it's own server (ah the irony of it)

--- src/routes/editor/[id]/+server.js
+++ src/routes/editor/[id]/+server.js
@@ -18,28 +18,22 @@ export async function POST({ request }) {
 
 export async function PUT({ request }) {
     await mongoose.connect("mongodb://127.0.0.1:27017/project");
-    const solution = await request.json();
-    let testcases = await Problem.find({ problemID: solution.id }, { _id: 0, testcases: 1 });
+    const { id, code, user } = await request.json();
+    let testcases = await Problem.find({ problemID: id }, { _id: 0, testcases: 1 });
     testcases = testcases.map(each => each.testcases)[0];
-    const dirName = "solutions";
-    const file = `${solution.id}_${solution.user}.py`;
-    if(!existsSync(dirName)) {
-        mkdirSync(dirName);
-    }
-    const filePath = `${dirName}/${file}`;
-    writeFileSync(filePath, solution.code);
 
-    let caseStatus = {};
 
-    for(let each = 0; each < testcases.length; each++) {
-        console.log(filePath);
-        const runProgram = spawnSync("docker", ["run", "--rm", "-i", "codecraft", "py", filePath ], { input: testcases[each].stdin });
-        //const runProgram = spawnSync("python3", [filePath], { input: testcases[each].stdin })
-        console.log(String(runProgram.stderr));
-        const userStdout = String(runProgram.stdout).trim();
-        console.log(userStdout);
-        caseStatus[each + 1] = (userStdout === testcases[each].stdout) ? "Passed" : "Failed";
-    }
-    console.log(caseStatus);
-    return json(caseStatus);
+    let metadata = { id, code, user, testcases };
+    const stdoutRequest = await fetch("http://127.0.0.1:3000/", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(metadata)
+    });
+    const stdoutResponse = await stdoutRequest.json();
+
+    console.log(stdoutResponse);
+    return json(stdoutResponse);
 }
