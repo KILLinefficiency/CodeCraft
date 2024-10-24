697cf7dab507f7785adc1af83fe75b77268f5be4
KILLinefficiency <shreyas.sable2166@gmail.com>
2024-10-24 22:30:30 +0530

    got somewhere with docker

--- src/routes/editor/[id]/+server.js
+++ src/routes/editor/[id]/+server.js
@@ -22,17 +22,22 @@ export async function PUT({ request }) {
     let testcases = await Problem.find({ problemID: solution.id }, { _id: 0, testcases: 1 });
     testcases = testcases.map(each => each.testcases)[0];
     const dirName = "solutions";
+    const file = `${solution.id}_${solution.user}.py`;
     if(!existsSync(dirName)) {
         mkdirSync(dirName);
     }
-    const filePath = `${dirName}/${solution.id}_${solution.user}.py`;
+    const filePath = `${dirName}/${file}`;
     writeFileSync(filePath, solution.code);
 
     let caseStatus = {};
 
     for(let each = 0; each < testcases.length; each++) {
-        const runProgram = spawnSync("python3", [filePath], { input: testcases[each].stdin })
+        console.log(filePath);
+        const runProgram = spawnSync("docker", ["run", "--rm", "-i", "codecraft", "py", filePath ], { input: testcases[each].stdin });
+        //const runProgram = spawnSync("python3", [filePath], { input: testcases[each].stdin })
+        console.log(String(runProgram.stderr));
         const userStdout = String(runProgram.stdout).trim();
+        console.log(userStdout);
         caseStatus[each + 1] = (userStdout === testcases[each].stdout) ? "Passed" : "Failed";
     }
     console.log(caseStatus);
