1c1b626aaaa3129d295644807884262ce7c73a7d
KILLinefficiency <shreyas.sable2166@gmail.com>
2025-03-03 16:47:42 +0530

    commented out an error causing line.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ import { skeleton } from "@skeletonlabs/tw-plugin";
 export default {
   darkMode: "class",
   content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte}")
+    "./src/**/*.{html,js,svelte,ts}"//,
+    //join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte}")
   ],
 
   theme: {
